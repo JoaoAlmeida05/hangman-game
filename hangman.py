3803662beff54473b86beb3a7176389fb321202c
JoaoAlmeida05 <joaovhomem@icloud.com>
2022-12-29 14:08:48 -0800

    second commit

--- hangman.py
+++ hangman.py
@@ -1,5 +1,6 @@
 from urllib.request import Request, urlopen
 import random
+import time
 
 #here we are chosing the word for the algorithm
 url="https://svnweb.freebsd.org/csrg/share/dict/words?revision=61569&view=co"
@@ -15,21 +16,31 @@ word = word.casefold()
 
 wordLength = len(word)
 
-indicate = ''
+indicate = []
 for i in range(wordLength):
-    indicate += '_|'
+    indicate += '_'
 
 
 #start the visual 
 def algthm(i):
     letter = input('Try a letter: ')
+    
     letter.strip()
     wrong = []
     
+    # add the words to the part that is supposed to be if u answered correct or missed
     if word.find(letter) == -1:
         i += 1
-        wrong.append(letter)       
+        wrong.append(letter) 
+    else: # make it go to the part that is correct, the letter should appear where it is in the string.
+        for a in range(word.count(letter)):
+            place = word.rfind(letter)
+            indicate[place] = letter
+            
 
+            
+
+    time.sleep(1)
 
     if i == 0:
         print("  ___________.._______ \n| .__________))______| \n| | / /      || \n| | | / \n| |/ \n| | \n| | | \n| | \n| | \n| | | \n| | \n| | \n| | | \n| | \n|=========|_`-' `-' |===| \n|=|=======\ \       '=|=| \n| |        \ \        | | \n: :         \ \       : : \n. .          `'       . . \n")
@@ -51,13 +62,20 @@ def algthm(i):
         print(indicate) 
     elif i == 6:
         print(" ___________.._______ \n| .__________))______| \n| | / /      || \n| |/ /       || \n| | /        ||.-''. \n| |/         |/  _  \ \n| |          ||  `/,| \n| |          (\\`_.' \n| |         .-`--'. \n| |        /Y . . Y\ \n| |       // |   | \\ \n| |      //  | . |  \\ \n| |     ')   |   |   (` \n| |          || || \n| |          || || \n| |          || || \n| |          || || \n| |         / | | \\ \n==========|_`-' `-' |===| \n|=|=======\ \       '=|=| \n| |        \ \        | | \n: :         \ \       : : \n. .          `'       . . \n")
-        print(indicate)     
+        print(indicate)    
+        time.sleep(2)
+        print('/// YOU LOST ///')
+
     print(word)
     
+    time.sleep(1)
     if len(wrong) != 0:
         print('Wrong letters: ' + str(wrong))
 
-        
+    if indicate.count('_') == 0:
+        print('/// YOU WON ///')
+        i = 6
+
     if i != 6:
         algthm(i)
     
